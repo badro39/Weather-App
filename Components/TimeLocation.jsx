42bbef3e593d3d8503602cb0c6f1dcc8fb504c00
Badro39 <badrdjab39@gmail.com>
2025-06-05 18:11:20 +0100

    fix time updating issue

--- Components/TimeLocation.jsx
+++ Components/TimeLocation.jsx
@@ -1,7 +1,8 @@
 // Styles
 import styles from "@/app/page.module.css";
+import { useEffect, useState } from "react";
 
-const time = new Date().toLocaleTimeString("en-US", {
+const timeFormat = new Date().toLocaleTimeString("en-US", {
   hour: "numeric",
   minute: "numeric",
   hour12: false, // Set to `true` for AM/PM format
@@ -14,6 +15,21 @@ const date = new Date().toLocaleDateString("en-US", {
 });
 
 const TimeLocation = ({ name, theme }) => {
+  const [time, setTime] = useState(timeFormat);
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(
+        new Date().toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: false,
+        })
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div
       className={`rounded-4 col-lg-4 col-md-5 col-sm-12 col-12 d-flex flex-column justify-content-center text-center ${styles.widgets}`}
